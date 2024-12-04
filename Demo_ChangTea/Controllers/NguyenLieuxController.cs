8eb9ed12188b2dc5dd6e677274c77c26f48231e1
TuanKiet1774 <167632480+TuanKiet1774@users.noreply.github.com>
2024-12-04 18:44:22 +0700

    commit

---
+++ Demo_ChangTea/Controllers/NguyenLieuxController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Demo_ChangTea.Models;
+
+namespace Demo_ChangTea.Controllers
+{
+    public class NguyenLieuxController : Controller
+    {
+        private QL_ChangTeaEntities db = new QL_ChangTeaEntities();
+
+        // GET: NguyenLieux
+        public ActionResult Index()
+        {
+            var nguyenLieux = db.NguyenLieux.Include(n => n.NhanVien);
+            return View(nguyenLieux.ToList());
+        }
+
+        // GET: NguyenLieux/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NguyenLieu nguyenLieu = db.NguyenLieux.Find(id);
+            if (nguyenLieu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nguyenLieu);
+        }
+
+        // GET: NguyenLieux/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaNV = new SelectList(db.NhanViens, "MaNV", "HoNV");
+            return View();
+        }
+
+        // POST: NguyenLieux/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaNL,MaNV,TenNL,SoLuong,DonVi,DonGia,NgSX,HSD")] NguyenLieu nguyenLieu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NguyenLieux.Add(nguyenLieu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaNV = new SelectList(db.NhanViens, "MaNV", "HoNV", nguyenLieu.MaNV);
+            return View(nguyenLieu);
+        }
+
+        // GET: NguyenLieux/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NguyenLieu nguyenLieu = db.NguyenLieux.Find(id);
+            if (nguyenLieu == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaNV = new SelectList(db.NhanViens, "MaNV", "HoNV", nguyenLieu.MaNV);
+            return View(nguyenLieu);
+        }
+
+        // POST: NguyenLieux/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaNL,MaNV,TenNL,SoLuong,DonVi,DonGia,NgSX,HSD")] NguyenLieu nguyenLieu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nguyenLieu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaNV = new SelectList(db.NhanViens, "MaNV", "HoNV", nguyenLieu.MaNV);
+            return View(nguyenLieu);
+        }
+
+        // GET: NguyenLieux/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NguyenLieu nguyenLieu = db.NguyenLieux.Find(id);
+            if (nguyenLieu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nguyenLieu);
+        }
+
+        // POST: NguyenLieux/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            NguyenLieu nguyenLieu = db.NguyenLieux.Find(id);
+            db.NguyenLieux.Remove(nguyenLieu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Demo_ChangTea/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_ChangTea.Models
+{
+    public class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+}
